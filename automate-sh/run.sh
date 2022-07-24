1c1166bd47fe188e5a901db101b9ac98b0561dc0
koko004 <koko004@gmail.com>
2022-07-24 06:11:03 +0200

    Update run.sh

--- automate-sh/run.sh
+++ automate-sh/run.sh
@@ -2,3 +2,9 @@
 
 mkdir -p ~/.docker-compose/pihole
 cd ~/.docker-compose/pihole
+docker-compose up
+git clone https://github.com/koko004/hassio-repo
+cd hassio repo
+cd automate-sh
+cd pi-hole
+docker-compose up
