6e50348428e67c7eb4f1f7aa81c0f0f611a79d1a
koko004 <koko004@gmail.com>
2022-09-24 09:46:11 +0200

    Delete automate-sh directory

--- automate-sh/run2.sh
+++
@@ -1,6 +0,0 @@
-
-#!/usr/bin/with-contenv bashio
-
-docker volume create pihole_app
-docker volume create dns_config
-docker run --name=pihole -e TZ=Europe/Madrid -e WEBPASSWORD=rododolfo -e SERVERIP=192.168.1.229 -v pihole_app:/etc/pihole -v dns_config:/etc/dnsmasq.d -p 88:80 -p 58:53/tcp -p 58:53/udp --restart=unless-stopped pihole/pihole
