952e3c40899905cf69198e464d785c7ae0c6126a
koko004 <koko004@gmail.com>
2023-08-10 20:36:57 +0200

    Update run.sh

--- walla/run.sh
+++ walla/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/with-contenv bashio
 
-docker run --env BOT_TOKEN=5901386566:AAHk5geSY-7CNy9FwVZjWdMfhX6hplAsb0s z0r3f/wallbot-docker:latest --name telebot
+docker run --env BOT_TOKEN=5901386566:AAHk5geSY-7CNy9FwVZjWdMfhX6hplAsb0s z0r3f/wallbot-docker:latest
