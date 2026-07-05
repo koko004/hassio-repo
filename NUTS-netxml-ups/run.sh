b00afc63f6f54f9b5610228484813a274820beca
koko004 <koko004@gmail.com>
2026-07-06 00:27:53 +0200

    Refactor run.sh to improve configuration generation

--- NUTS-netxml-ups/run.sh
+++ NUTS-netxml-ups/run.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 OPTIONS_FILE="/data/options.json"
 
-# Extraer parámetros de Home Assistant
 DEVICE_NAME=$(jq --raw-output '.device_name' $OPTIONS_FILE)
 DRIVER=$(jq --raw-output '.driver' $OPTIONS_FILE)
-# Limpiar el puerto: asegurar que no tenga http:// ni barras
 PORT=$(jq --raw-output '.port' $OPTIONS_FILE | sed 's|http://||g' | sed 's|/||g')
 USERNAME=$(jq --raw-output '.username' $OPTIONS_FILE)
 PASSWORD=$(jq --raw-output '.password' $OPTIONS_FILE)
 
-# Generar configuración
 cat << EOF > /etc/nut/ups.conf
 [$DEVICE_NAME]
     driver = $DRIVER
@@ -31,13 +28,10 @@ EOF
 cat << EOF > /etc/nut/upsmon.conf
 MONITOR $DEVICE_NAME@localhost 1 admin $PASSWORD primary
 SHUTDOWNCMD "/sbin/shutdown -h now"
-POWERDOWNFLAG /etc/killpower
 EOF
 
 chmod 640 /etc/nut/*
 
-# Ejecución
-echo "[INFO] Iniciando driver $DRIVER..."
 /usr/libexec/nut/$DRIVER -D -a $DEVICE_NAME &
 sleep 5
 /usr/sbin/upsd -D &
